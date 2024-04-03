1b5883ef5d4826bbe52c0d12af6e71f336b58ed6
y34hh <yourenot52@gmail.com>
2024-04-03 19:36:08 +0700

    костя чини

---
+++ laba4-1/laba4-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] steps = { 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8 };
+
+        foreach (var step in steps)
+        {
+            double x = 1.0; // Точка, в которой вычисляется производная
+            double h = step; // Шаг
+
+            double derivative = CalculateDerivative(x, h);
+
+            Console.WriteLine($"Шаг: {step}, Первая производная: {derivative}");
+        }
+
+        Console.ReadLine();
+    }
+
+    static double Function(double x)
+    {
+        return 3 * x * x;
+    }
+
+    static double CalculateDerivative(double x, double h)
+    {
+        double fxph = Function(x + h); // f(x + h)
+        double fxmh = Function(x - h); // f(x - h)
+
+        double derivative = (fxph - fxmh) / (2 * h);
+
+        return derivative;
+    }
+}
